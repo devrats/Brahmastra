d20e98699fd6bdd2a434a2e3efc447079186f944
Devrats <devvratsharma9759@gmail.com>
2021-09-10 11:55:58 +0530

    Initial commit

---
+++ src/test/java/com/example/brahmastra/BrahmastraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brahmastra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrahmastraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
