3442b9f5a907b074693858deb56454f77a1019f6
Devrats <devvratsharma9759@gmail.com>
2021-09-10 12:01:04 +0530

    prepared basic template

---
+++ src/main/java/com/example/brahmastra/controller/HomeController.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devvrat Sharma (devrats)
+ *   Date: 10-Sep-21
+ *   Time: 11:59 AM
+ *   File: HomeController.java
+ */
+
+package com.example.brahmastra.controller;
+
+public class HomeController {
+}
---
+++ src/main/java/com/example/brahmastra/entity/Client.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devvrat Sharma (devrats)
+ *   Date: 10-Sep-21
+ *   Time: 12:00 PM
+ *   File: Client.java
+ */
+
+package com.example.brahmastra.entity;
+
+public class Client {
+}
